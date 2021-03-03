f25f89921d1a9f854c964d3a5e72ece7e24ca399
Alex <alexander.korpas@protonmail.com>
2021-03-03 10:49:58 +0100

    pw changer

--- Kapitel1/Uppgift2.3/PasswordChanger.cs
+++ Kapitel1/Uppgift2.3/PasswordChanger.cs
@@ -21,27 +21,37 @@ namespace Uppgift2._3
             {
                 Console.WriteLine("Enter your old password: ");
                 input = Console.ReadLine();
+                Console.WriteLine();
                 if (input != pwc.password)
-                    Console.WriteLine("Wrong password. Try again.");
+                    Console.WriteLine("Wrong password. Try again.\n");
                 else
                     break;
                     
             }
+            // if input matches the user's password we continue
             if(input == pwc.password)
             {
-                Console.WriteLine("Enter new password: ");
                 while (true)
                 {
+                    Console.WriteLine("Enter new password: ");
                     string newPw = Console.ReadLine();
+                    // check the new password against the criteria, repeat process if insufficient quality
                     if (pwc.checkPassword(newPw))
                     {
-                        Console.WriteLine("Your password has been changed. Enjoy.");
-                        break;
+                        // repeat the new password to confirm change
+                        Console.WriteLine("Repeat password: ");
+                        string repeatPw = Console.ReadLine();
+                        // passwords don't match - reloop
+                        if (repeatPw != newPw)
+                            Console.WriteLine("Passwords don't match. Try again.");
+                        else
+                            break;
                     }
                     else
                         Console.WriteLine("Password needs to be at least 8 characters long,\n" +
-                            "contain a number, a special character.\nTry again.");
+                            "contain a number and include a special character.\nTry again.\n");
                 }
+                Console.WriteLine("Your password has been changed. Enjoy.");
             }
         }
 
@@ -56,8 +66,8 @@ namespace Uppgift2._3
             Regex rgx = new Regex(@"^[a-zA-Z0-9_@.-]*$");
             bool lengthOk = pw.Length >= 8;
             bool containsNumber = pw.Any(char.IsDigit);
+            // check if the password contains some symbol other than aA-zZ-0-9
             bool containsSpecialChar = !rgx.IsMatch(pw);
-            Console.WriteLine(containsSpecialChar);
             bool containsUppercase = pw.Any(char.IsUpper);
             bool containsLowercase = pw.Any(char.IsLower);
             return lengthOk && containsNumber && containsSpecialChar && containsUppercase && containsLowercase;
